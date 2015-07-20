90d2c08f9798db0e63e71416daadf665c498386f
David Jay <davidjay@samsungaccelerator.com>
2015-07-20 19:38:12 -0400

    Parsing tweet info and sending as event.

--- twittertracker.js
+++ twittertracker.js
@@ -1,6 +1,7 @@
 var Twitter = require('node-tweet-stream'),
 request = require("http").request,
-async = require("async");
+async = require("async")
+EventEmitter = require("events");
 
 var Tracker = function() {
 	this.tracker = new Twitter({
@@ -12,13 +13,25 @@ var Tracker = function() {
 };
 
 Tracker.prototype.start = function() {
+	var ee = new EventEmitter();
 	this.tracker.on('tweet', function (tweet) {
-  		console.log('tweet received', tweet)
-
-  		//TODO:Increment count
-  		//TODO:Parse hashtags
-  		//TODO:Parse users
-  		//Probably implement this by sending an event w/ vital tweet info: hashtag, author, mentions.
+  		var hashtags=[];
+  		for (var i = tweet.entities.hashtags.length - 1; i >= 0; i--) {
+  			hashtags.push(tweet.entities.hashtags[i].text);
+  		};
+  		var users=[];
+  		 for (var i = tweet.entities.user_mentions.length - 1; i >= 0; i--) {
+  			users.push(tweet.entities.user_mentions[i].screen_name);
+  		};
+  		var tweet_data={
+  			author:tweet.user.screen_name,
+  			mentions:{
+  				hashtags:hashtags,
+  				users:users
+  			}
+  		}
+  		console.log(JSON.stringify(tweet_data));
+  		ee.emit("tweet",tweet_data);
 	});
 	this.tracker.on('error', function (err) {
   		console.log('Oh no')
