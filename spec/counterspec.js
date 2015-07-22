21cd044a52608fb4ccc9a49b7923bf45a947d91d
David Jay <davidjay@samsungaccelerator.com>
2015-07-22 16:12:01 -0400

    Parsing tweets and sending to firebase.

--- spec/counterspec.js
+++ spec/counterspec.js
@@ -3,26 +3,60 @@ var EventEmitter = require("events"),
 Counter = require('../tweetcounter.js');
 
 describe("Counter", function() {
+
+	var counter,
+	ee,
+	mockTweet = {
+		author:"davidgljay",
+		mentions:{
+			hashtags:["asexuality","climatechange"],
+			users:["nmebrown","stampisaur"]
+		}
+	};
+
+	describe ("adding, removing and clearing", function() {
+
+		beforeEach(function() {
+			ee = new EventEmitter();
+			counter = new Counter(ee);
+		});
+
+		it ("should add a hashtag", function() {
+			counter.add("asexuality");
+			expect(counter.tracked_hashtags.asexuality).not.toBeNull();
+			expect(counter.tracked_hashtags.climatechange).toBe(undefined);	  	
+		})
+
+		it ("should remove a hashtag", function() {
+			counter.add("asexuality");
+			counter.remove("asexuality");
+			expect(counter.tracked_hashtags.asexuality).toBe(undefined); 	
+		})
+
+		it ("should clear existing hashtags", function() {
+			counter.add("asexuality");
+			ee.emit('tweet',mockTweet);
+			expect(counter.tracked_hashtags.asexuality.count).toBe(1);
+			counter.clear();
+			expect(counter.tracked_hashtags.asexuality.count).toBe(0);
+		})
+
+	})
+
 	describe ("receiving a tweet", function() {
-
-		var counter;
-		var ee = new EventEmitter();
-
-		var mockTweet = {
-			author:"davidgljay",
-			mentions:{
-				hashtags:["asexuality","climatechange"],
-				users:["nmebrown","stampisaur"]
-
-			}
-		};
 
 
 		beforeEach(function() {
+			ee = new EventEmitter();
 			counter = new Counter(ee);
 			counter.add("asexuality");
 			ee.emit('tweet',mockTweet);
 		});
+
+		afterEach(function() {
+			counter.stop();
+			counter.clear();
+		})
 
 		it ("should add a hashtag", function() {
 			expect(counter.tracked_hashtags.asexuality).not.toBeNull();
@@ -61,5 +95,22 @@ describe("Counter", function() {
 			expect(counter.tracked_hashtags.asexuality.mentions.users.nmebrown).toBe(2);
 		})
 	})
+
+	// describe("scheduling updates to firebase", function() {
+	// 	beforeEach(function() {
+	// 		counter = new Counter(ee);
+	// 		counter.add("asexuality");
+	// 		ee.emit('tweet',mockTweet);
+	// 	});
+
+	// 	it ("should start the update cycle without registering errors", function() {
+	// 		counter.startUpdateCycle();
+	// 	})
+
+	// 	it ("should perform an update without registering errors", function() {
+	// 		counter.update();
+	// 	})
+
+	// })
 
 });
--- tweetcounter.js
+++ tweetcounter.js
@@ -1,19 +1,16 @@
 var EventEmitter = require("events"),
-Firebase = require('firebase'),
-_=require('underscore');
+Firebase = require('firebase');
 
 var Counter = function(ee) {
 	var self=this;
 	self.tracked_hashtags={};
 	self.ee = ee;
-	self.ee.on('tweet', function(tweet_data) {
 
+	self.parseTweet = function(tweet_data) {
 		for (var i = tweet_data.mentions.hashtags.length - 1; i >= 0; i--) {
 			if (self.tracked_hashtags[tweet_data.mentions.hashtags[i]]) {
 				var hashtag = tweet_data.mentions.hashtags[i];
 
-
-				//Write tests
 				//Incremend the index;
 				self.tracked_hashtags[hashtag].count += 1;
 
@@ -44,24 +41,85 @@ var Counter = function(ee) {
 					}
 				};
 			};
-
 		};
-	})
+	}
+	self.ee.on('tweet', self.parseTweet);
 }
 
-Counter.prototype.add = function(hashtag) {
-	this.tracked_hashtags[hashtag] = {
+Counter.prototype.blankHashTag = function() {
+	return {
 		count:0,
 		mentions: {
 			hashtags: {},
 			users: {}
 		},
 		authors:{}
-	};
+	}
+}
+
+Counter.prototype.add = function(hashtag) {
+	var self=this;
+	self.tracked_hashtags[hashtag] = self.blankHashTag();
 };
 
 Counter.prototype.remove = function(hashtag) {
-	//TODO:splice tracked_hashtags array to remove hashtag
+	var self = this;
+	delete self.tracked_hashtags[hashtag]
 }
+
+Counter.prototype.clear = function() {
+	var self = this;
+	for (hashtag in  self.tracked_hashtags) {
+		self.remove(hashtag);
+		self.add(hashtag);
+	};
+}
+/*
+Update firebase on the hour.
+*/
+Counter.prototype.millisTillNextHour = function() {
+	var d = new Date();
+	d.setMinutes(d.getMinutes()+1);
+	d.setSeconds(0);
+	d.setMilliseconds(0);
+	// d.setHours(d.getHours()+1);
+	return d - new Date();
+}
+
+Counter.prototype.startUpdateCycle = function() {
+	var self=this;
+	var scheduleUpdate = function() {
+		setTimeout(function() {
+			self.update();
+			scheduleUpdate();
+			console.log("Scheduling update");
+		}, self.millisTillNextHour());
+	};
+	scheduleUpdate();
+}
+
+Counter.prototype.update = function() {
+	var self=this;
+	var fbHashtags = new Firebase(process.env.FIREBASE_URL + '/users/testUser/');
+	for (hashtag in self.tracked_hashtags) {
+		var time = Date.now().toString();
+		fbHashtags.child(hashtag).child(time).set(self.tracked_hashtags[hashtag]);
+		console.log('Pushing hashtag:' + hashtag + "\n");
+	};
+	self.clear();
+}
+
+
+Counter.prototype.start = function() {
+	var self=this;
+	self.ee.on('tweet', self.parseTweet);
+}
+
+Counter.prototype.stop = function() {
+	var self=this;
+	self.ee.removeListener("tweet",self.parseTweet);
+}
+
+
 
 module.exports = Counter;
